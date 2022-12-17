f6c1c3f2d1e802207f22333b27df0ab07af431b3
Ada <uzodimmaada@gmail.com>
2022-12-16 19:14:02 -0800

    NUMBERS

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers- Entry
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = '0'; n <= '9'; n++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		if (x != 2 && x != 4)
+		{
+			_putchar(x + '0');
+		}
+	}
+	_putchar('\n');
+}
