bef037f18b2e1863b3cff100025532e9450b3b77
Ada <uzodimmaada@gmail.com>
2022-12-20 12:58:55 -0800

    pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - Entry
+ * @s: print
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	for (i -= 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
