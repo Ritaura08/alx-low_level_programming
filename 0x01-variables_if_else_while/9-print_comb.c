8f741b5bfb636d2157f72f6f14e2dcdda64eb444
Ada <uzodimmaada@gmail.com>
2023-01-02 15:07:46 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,3 +4,21 @@
  * main - entry
  * Return: Alwaysn0
  */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
