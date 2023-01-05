5a6d0eedd0d9c7dc9fb8631f55a865309a09ca64
Ada <uzodimmaada@gmail.com>
2023-01-04 19:30:41 -0800

    pointers and arrays

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,9 +11,9 @@ void print_chessboard(char (*a)[8])
 	int k;
 	int m;
 
-	for (k = 0, k < 8; k++)
+	for (k = 0; k < 8; k++)
 	{
-		for (m = 0, m < 8, m++)
+		for (m = 0; m < 8; m++)
 			_putchar(a[k][m]);
 		_putchar('\n');
 	}
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - Entry
+ * @a: ch
+ * @size: ch
+ * Return: 0
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int sum1, sum2, i;
+
+	sum1 = 0;
+	sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i * size + i];
+		sum2 += a[i * size + (size - i - 1)];
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
