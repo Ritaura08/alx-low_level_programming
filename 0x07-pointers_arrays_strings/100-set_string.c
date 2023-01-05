502b0c94beb8cf8d3bda545f6313fab8dc64dc48
Ada <uzodimmaada@gmail.com>
2023-01-04 19:05:07 -0800

    pointers and arrays

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main - entry
+ * Return: Always 0
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Entry
+ * @s: ch
+ * @accept: ch
+ * Return: 0
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int m;
+
+	while (*s)
+	{
+		for (m = 0; accept[m]; m++)
+		{
+			if (*s == accept[m])
+				return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
