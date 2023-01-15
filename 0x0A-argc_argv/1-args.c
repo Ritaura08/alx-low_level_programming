02665ccc1cbffe82269bfc1c6cfbfdec68587dc4
Ada <uzodimmaada@gmail.com>
2023-01-14 19:12:29 -0800

    argc_argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - Entry
+  * @argc: argc
+  * @argv: argv
+  * Return: 0
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
