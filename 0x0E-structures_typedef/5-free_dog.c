a6a84789ac74c482b6554225d0fb1b46cf219e05
Ada <uzodimmaada@gmail.com>
2023-01-17 20:02:43 -0800

    structures and typedef

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,16 +1,16 @@
 #include "dog.h"
 
 /**
- * free_dog - Frees dog struct
+ * free_dog - Entry
  * @d: Dog structure pointer
  */
 
 void free_dog(dog_t *d)
 {
-	if (d)
-	{	
+	if (d == NULL)
+		return;
+
 		free(d->name);
 		free(d->owner);
 		free(d);
-	}
 }
